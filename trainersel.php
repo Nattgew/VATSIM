c54376edd20d58b00ce2b1e83349f5471c8a8efc
Nattgew <Nattgew@users.noreply.github.com>
2019-12-20 21:37:28 -0800

    Merge with local, add basic scripting

--- trainersel.php
+++ trainersel.php
@@ -5,7 +5,7 @@
 <title>TWRTrainer Generator</title>
 <meta name="description" content="TWRTrainer Generator">
 <meta name="author" content="N">
-<!--<link rel="stylesheet" href="style.css">-->
+<link rel="stylesheet" href="style.css">
 <style>
 .rates {
   width: 40px;
@@ -16,40 +16,72 @@
   padding-left:15px;
 }
 </style>
-<script>
-
-document.getElementById("mytext").value = "My value";
+<script type="text/javascript">
+function load_gnd() {
+  document.getElementById("chance_ec").value = "0.5";
+  document.getElementById("chance_alt").value = "0.25";
+  document.getElementById("chance_dest").value = "0.01";
+  document.getElementById("chance_route").value = "0.25";
+  document.getElementById("chance_rules").value = "0.1";
+  document.getElementById("chance_route_swap").value = "0.1";
+  document.getElementById("chance_route_dct").value = "0.05";
+  document.getElementById("chance_route_blk").value = "0.05";
+}
+function load_twr() {
+  document.getElementById("chance_ec").value = "0.01";
+  document.getElementById("chance_alt").value = "0.01";
+  document.getElementById("chance_dest").value = "0.001";
+  document.getElementById("chance_route").value = "0.01";
+  document.getElementById("chance_rules").value = "0.01";
+  document.getElementById("chance_route_swap").value = "0.1";
+  document.getElementById("chance_route_dct").value = "0.05";
+  document.getElementById("chance_route_blk").value = "0.05";
+}
 </script>
 </head>
+<body>
 <h2>Select an airport:</h2>
-
+<form method="post" id="gen_form" action="/twrtrainerapt.php">
 <p>
-<input type="radio" name="icao" value="KPDX" checked="checked"/> KPDX<br/>
-<input type="radio" name="icao" value="KSEA"/> KSEA<br/>
-<input type="radio" name="icao" value="KOTH"/> KOTH
+<?php
+# Whitelisting these, could also scan existing files
+$airports = array("KSEA", "KPDX", "KTTD", "KHIO", "KOTH");
+
+foreach ($airports as $airport) {
+  if (file_exists("twrtrainer/".$airport.".apt")) {
+    if ($airport == "KPDX") {
+      $chk = ' checked="checked"';
+    } else {
+      $chk = '';
+    }
+    echo '<input type="radio" name="icao" value="'.$airport.'"'.$chk.'/> '.$airport.'<br/>';
+  }
+}
+?>
 </p>
+<p>Initial number of aircraft: <input type="text" name="actoadd" style="width:20px;" value="5"/></p>
 
 <h3>Error Rates</h3>
 <p>
-<input type="button" name="gnd_em" value="Load GND emphasis"> <input type="button" name="twr_em" value="Load TWR emphasis"> 
+<input type="button" name="gnd_em" id="gnd_em" value="Load GND emphasis" onclick="load_gnd()"/>
+<input type="button" name="twr_em" id="twr_em" value="Load TWR emphasis" onclick="load_twr()"/>
 </p>
 <!--<p style="width:200px;">-->
 <!-- TODO: Loop this -->
-<form action="" name="prob_form">
 <table>
 <tr><th>Type</th><th>Rate/1</th></tr>
 <tr><td>Equipment code:</td><td><input type="text" name="chance_ec" id="chance_ec" value="0.5" class="rates"/></td></tr>
 <tr><td>Altitude:</td><td><input type="text" name="chance_alt" id="chance_alt" value="0.25" class="rates"/></td></tr>
 <tr><td>Destination:</td><td><input type="text" name="chance_dest" id="chance_dest" value="0.01" class="rates"/></td></tr>
-<tr><td>Flight rules:</td><td><input type="text" name="chance_rules" name="chance_rules" value="0.1" class="rates"/></td></tr>
-<tr><td>Route:</td><td><input type="text" name="chance_route" value="0.25" class="rates"></td/></tr>
+<tr><td>Flight rules:</td><td><input type="text" name="chance_rules"  id="chance_rules" value="0.1" class="rates"/></td></tr>
+<tr><td>Route:</td><td><input type="text" name="chance_route" id="chance_route" value="0.25" class="rates"></td/></tr>
 <!--<p style="padding-left:20px;">-->
-<tr><td class="sub">Total swap:</td><td><input type="text" name="chance_route_swap" value="0.1" class="rates"/></td></tr>
-<tr><td class="sub">DCT:</td><td><input type="text" name="chance_route_dct" value="0.05" class="rates"/></td></tr>
-<tr><td class="sub">Blank:</td><td><input type="text" name="chance_route_blk" value="0.05" class="rates"/></td></tr>
+<tr><td class="sub">Total swap:</td><td><input type="text" name="chance_route_swap" id="chance_route_swap" value="0.1" class="rates"/></td></tr>
+<tr><td class="sub">DCT:</td><td><input type="text" name="chance_route_dct" id="chance_route_dct" value="0.05" class="rates"/></td></tr>
+<tr><td class="sub">Blank:</td><td><input type="text" name="chance_route_blk" id="chance_route_blk" value="0.05" class="rates"/></td></tr>
 </table>
-<p><input type="submit" value="Generate Aircraft">
-</p>
+<p><input type="submit" value="Generate Aircraft"></p>
 </form>
+
 </body>
 </html>
